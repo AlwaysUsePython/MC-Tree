0c36816a695098d5dd52ed718ad54ddbb15bca2d
ElliotLichtman <elliot.joseph.lichtman@gmail.com>
2022-10-09 18:34:47 -0700

    Untested MCTree

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ import numpy as np
 # - a total score
 # - a counter to keep track of the number of visits
 # - a method to calculate UCBI
+# - a method that expands a node
+# - a method that finds highestUCBI
 
 # note: what is UCBI? It's a way to balance between exploration and exploitation.
 # Exploitation side is the average value (self.total/self.visits)
@@ -24,7 +26,7 @@ import numpy as np
 # UCBI is sum of Exploitation value + Exploration value
 class GameState:
 
-    def __init__(self, board, player):
+    def __init__(self, board, player = None):
         self.board = board
         self.total = 0
         self.visits = 0
@@ -35,6 +37,7 @@ class GameState:
 
     def setParent(self, parentState):
         self.parent = parentState
+        self.player = getNextPlayer(parentState.player)
         parentState.addChild(self)
 
     def addChild(self, childState):
@@ -92,18 +95,167 @@ class GameState:
 
         return childScores
 
+    def findHighestUCBILeaf(self):
+
+        # base case:
+        if self.visits == 0 and self.parent != None:
+            return self
+
+        # recursive step(s):
+        if self.children != []:
+            self.expand()
+            return self.children[0]
+
+        else:
+            UCBIs = self.getChildrenUCBIs()
+            favoriteChild = 0
+            max = UCBIs[0]
+
+            for childNum in range(len(UCBIs)):
+                if UCBIs[childNum] > max:
+                    favoriteChild = childNum
+                    max = UCBIs[childNum]
+
+            return self.children[favoriteChild]
+
+
+    def expand(self):
+        nextMoves = getNextMoves(self.board, getNextPlayer(self.player))
+
+        for nextState in nextMoves:
+            newState = GameState(getBoardCopy(nextState))
+            newState.setParent(self)
+
 # Things a MCTree needs:
 # - root node -> this will point to all the other nodes
 # - iterate() function that updates the values
 # - makeChoice(x) function that iterates x times and picks out the best branch
 
 # note: to write this, we need to assume that a few game-specific functions exist
-# 1) getNextMoves(currentBoard, player)
 
 class MCTree:
 
     def __init__(self, start):
 
         # note: this root is likely going to be a part of a different tree. So we can't just reuse it – we need to reset the total and visits
-        self.root = GameState(start.board)
+        self.root = GameState(start.board, start.player)
+        self.root.expand()
 
+    def iterate(self):
+        leafToUpdate = self.root.findHighestUCBILeaf()
+
+        score = simulate(leafToUpdate.board, getNextPlayer(leafToUpdate.player))
+
+        leafToUpdate.addToTotal(score)
+        leafToUpdate.addVisit()
+
+    def makeChoice(self, iterations):
+        for iteration in range(iterations):
+            self.iterate()
+
+        childScores = []
+
+        for child in self.root.children:
+            childScores.append(child.total / child.visits)
+
+        approved = 0
+        max = childScores[0]
+
+        for item in range(len(childScores)):
+            if childScores[item] > max:
+                approved = item
+
+        return [self.root.children[approved].board, max]
+
+
+
+
+
+
+
+
+
+## GAME SPECIFIC FUNCTIONS ## (right now tic tac toe)
+
+# Takes in the currentPlayer, returns the next player
+def getNextPlayer(currentPlayer):
+    if currentPlayer == 'X':
+        return 'O'
+
+    return 'X'
+
+# Takes in the board and the player, returns a list of BOARDS of the next possible game states
+def getNextMoves(currentBoard, player):
+    nextMoves = []
+
+    for row in range(3):
+        for col in range(3):
+            if currentBoard[row][row] == '.':
+                boardCopy = getBoardCopy(currentBoard)
+                boardCopy[row][col] = player
+                nextMoves.append(boardCopy)
+
+    return nextMoves
+
+# Copies the board (duh)
+def getBoardCopy(board):
+    boardCopy = []
+
+    for row in board:
+        boardCopy.append(row.copy())
+
+    return boardCopy
+
+# takes in a board and a player, returns a score (positive if the player won, negative otherwise)
+def simulate(currentBoard, player):
+    currentPlayer = player
+    boardCopy = getBoardCopy(currentBoard)
+
+    simulationMoves = []
+    nextMoves = getNextMoves(boardCopy, currentPlayer)
+
+    score = 100
+
+    while nextMoves != []:
+        roll = random.randint(1, len(nextMoves)) - 1
+        boardCopy = nextMoves[roll]
+
+        simulationMoves.append(boardCopy)
+
+        if hasWon(boardCopy, currentPlayer):
+            break
+
+        score -= 1
+
+        player = getNextPlayer(currentPlayer)
+        nextMoves = getNextMoves(boardCopy, currentPlayer)
+
+    if player != currentPlayer and hasWon(boardCopy, player):
+        score *= -1
+
+    return score
+
+# Checks if someone has won (just for the input player)
+def hasWon(currentBoard, player):
+    winningSet = [player for _ in range(3)]
+
+    for row in currentBoard:
+        if row == winningSet:
+            return True
+
+    for y in range(len(currentBoard)):
+        column = [currentBoard[index][y] for index in range(3)]
+
+        if column == winningSet:
+            return True
+
+    diagonal1 = []
+    diagonal2 = []
+    for index in range(len(currentBoard)):
+        diagonal1.append(currentBoard[index][index])
+        diagonal2.append(currentBoard[index][3 - index - 1])
+
+    if diagonal1 == winningSet or diagonal2 == winningSet:
+        return True
+
+    return False
